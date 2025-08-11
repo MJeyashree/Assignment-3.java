811b125f962429d80e1bb1989a948d3007c3fb23
MJeyashree <167738023+MJeyashree@users.noreply.github.com>
2025-08-11 09:57:48 -0700

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -153,96 +153,6 @@ class VehicleMain{
 	}
 }
 
-/*5.Design a simple inventory system in Java using object-oriented principles that demonstrates the use of static variables and object 
-containment.
-Create two classes:
----Store
----Product
-Requirements:
-The Store class should have:
-a. storeName and storeLocation as static variables since they are common to all products in the store.
-b. A static method setStoreDetails(String name, String location) to initialize the static variables.
-c. A static method displayStoreDetails() to print store details.
-d. A list to maintain multiple Product objects (i.e., the store contains many products).
-e. A method addProduct(Product product) to add products to the store.
-f. A method displayAllProducts() to display details of all products.
-The Product class should have:
-a. Product ID, name, price, and quantity as instance variables.
-b. A constructor to initialize these fields.
-c. A method displayProduct() to show product details.
-Task:
-Implement the above classes and demonstrate their use in the main() method by:
-a. Setting store details once.
-b. Creating multiple product objects.
-c. Adding them to the store.
-d. Displaying store and product information.
-Also Check how many .class files are generated.*/
-
-import java.util.ArrayList;
-class Store{
-	static String storeName;
-	static String storeLocation;
-	private ArrayList<Product>products;
-	public Store(){
-		products=new ArrayList<>();
-	}
-	public static void setStoreDetails(String name,String location){
-		storeName=name;
-		storeLocation=location;
-	}
-	public static void displayStoreDetails(){
-		System.out.println("Store Name : " + storeName);
-		System.out.println("Store Location : " + storeLocation);
-	}
-	public void addProduct(Product product){
-		products.add(product);
-    }
-    public void displayAllproducts(){
-		System.out.println("List of Products : ");
-		for(Product p : products){
-			p.displayProduct();
-		}
-	}
-}
-class Product{
-	private int productID;
-	private String name;
-	private double price;
-	private int quantity;
-	public Product(int productID,String name,double price,int quantity){
-		this.productID=productID;
-		this.name=name;
-		this.price=price;
-		this.quantity=quantity;
-	}
-	public void displayProduct(){
-		System.out.println("Product ID : " + productID);
-		System.out.println("Product Name : " + name);
-		System.out.println("Product's price : " + price);
-		System.out.println("Quantity : " + quantity);
-		System.out.println("================================");
-	}
-}
-public class MainStore{
-	public static void main(String[] args){
-		Store.setStoreDetails("Big Bazzar","Chennai");
-		Store bigStore=new Store();
-		Product p1 = new Product(201,"Chocolates",150.48,20);
-		Product p2 = new Product(201,"Cookies",120.05,32);
-        Product p3 = new Product(201,"Fruits",500,10);
-        Product p4 = new Product(201,"Vegetables",230,15);
-        Product p5 = new Product(201,"Nuts",1000,30);
-		bigStore.addProduct(p1);
-		bigStore.addProduct(p2);
-		bigStore.addProduct(p3);
-		bigStore.addProduct(p4);
-		bigStore.addProduct(p5);
-		Store.displayStoreDetails();
-		bigStore.displayAllproducts();
-	}
-}
-
- 
 
 
 /* 5..Write a program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 
@@ -527,3 +437,4 @@ Choose the options of the list :
 0 Exit
 1
 Length: 10 */
+
